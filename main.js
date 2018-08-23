93b7b8aa39e02f0e2c048018ef53f3c691527b84
tzanuttini <gastonzanuttini@gmail.com>
2018-08-23 15:56:47 -0300

    tongarias

---
+++ main.js
@@ -0,0 +1,5 @@
+var imagenes = ["img/perro.jpg","img/arbol.jpg","img/auto.png","img/flor.png","img/gato.jpg","img/hombre.png","img/mujer.png","pajaro.jpg"];
+var intentos=10;
+var img = document.getElementsByClassName("card");
+var posicion;
+var maximo=2;
