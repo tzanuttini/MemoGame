0dcda810fd67d3c24299b1a9d383633ca0ab578b
tzanuttini <gastonzanuttini@gmail.com>
2018-08-29 19:56:57 -0300

    otra vesion

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-var imagenes = ["img/perro.jpg","img/arbol.jpg","img/auto.png","img/flor.png","img/gato.jpg","img/hombre.png","img/mujer.png","pajaro.jpg"];
-var intentos=10;
+var imagenes = ["img/perro.jpg","img/arbol.jpg","img/auto.png","img/flor.png","img/gato.jpg","img/hombre.png","img/mujer.png","img/pajaro.jpg"];
+var intentos=20;
 var img = document.getElementsByClassName("card");
-var posicion;
 var maximo=2;
+var card1="";
+var card2 = "";
+
+function mostrar(img){
+    if(card1 == "" ){
+        //card1=img;
+        img.src = imagenes[img.getAttribute('data-value')];
+    }
+}
