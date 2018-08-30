cc09956d9993dddc61aee9c9b12792ff58b03c36
tzanuttini <gastonzanuttini@gmail.com>
2018-08-29 22:52:02 -0300

    otra mas

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var img = document.getElementsByClassName("card");
 var maximo=2;
 var card1="";
 var card2 = "";
+var def = "img/preg.png";
 
 function mostrar(img){
     if(card1 == "" ){
@@ -12,5 +13,16 @@ function mostrar(img){
     }else if (card2 == ""){
         card2 = img;
         img.src = imagenes[img.getAttribute('data-value')];
+        if(card1.src != card2.src){
+            intentos--;
+            setTimeout(function(){
+                card1.src=def;
+                card2.src=def;
+                card1 = "";
+                card2 = "";
+            },2000);
+        }else{
+            intento--;
+        }
     }
 }
